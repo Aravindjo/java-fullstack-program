a646157d7bd1a8ff49abed3dc459955c1f77cec3
Aravindjo <rvdjo.7@gmail.com>
2020-10-06 16:25:21 +0530

    intro

---
+++ Maven.java/src/main/java/fullstack/java/Maven/java/App.java
@@ -0,0 +1,14 @@
+package fullstack.java.Maven.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {  
+    	System.out.println("welcome boys");
+    }
+}
+    
